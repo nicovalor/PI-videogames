5db1136a860e338eda6c7211a77813c1b229fb85
Nicolás Valor <nicoo.valor@gmail.com>
2023-02-10 23:18:13 -0300

    models started

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,11 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("@sequelize/core");
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+    launchDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTERGER,
       allowNull: false,
     },
   });
