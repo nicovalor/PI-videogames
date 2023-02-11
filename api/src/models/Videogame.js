c0d9d5adaab1b1e9d7bcdd6916615b3154e4b7ce
Nicolás Valor <nicoo.valor@gmail.com>
2023-02-11 17:40:41 -0300

    Base de datos y modelos creados

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,39 +1,44 @@
-const { DataTypes } = require("@sequelize/core");
+const { DataTypes } = require("sequelize");
 
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("videogame", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      launchDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.INTERGER,
+        allowNull: false,
+      },
     },
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    platforms: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.BLOB,
-      allowNull: false,
-    },
-    launchDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    rating: {
-      type: DataTypes.INTERGER,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
 };
