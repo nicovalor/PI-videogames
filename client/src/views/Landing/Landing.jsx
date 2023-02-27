809599f48373ae7230c37474bb7c5de672c73c7e
Nicolás Valor <nicoo.valor@gmail.com>
2023-02-27 19:36:45 -0300

    Landing page estilos

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <div className={style.container}>
-      <Link to="/home">
-        <button>ENTRAR</button>
-      </Link>
-      <div className={style.logo}>
-        <img src="" alt="Page logo" />
+      <div className={style.titleContainer}>
+        <h1 className={style.title}>Videogames</h1>
+        <h3 className={style.subtitle}>Proyecto individual - Nicolás Valor</h3>
       </div>
+      <Link to="/home">
+        <button className={style.button}>ENTRAR</button>
+      </Link>
     </div>
   );
 }
